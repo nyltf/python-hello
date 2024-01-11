fe765521478114b3e32c93ec17b9d0e01c05e691
nyltf <156117583+nyltf@users.noreply.github.com>
2024-01-11 10:56:37 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 import dash
+import os
+from wsgiref.simple_server import make_server
 
 app = dash.Dash(__name__)
 
 app.layout = dash.html.Div('Hello world!')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+#    app.run(debug=False)
+    port = int(os.environ.get("PORT"))
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
