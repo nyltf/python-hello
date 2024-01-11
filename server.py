5f1a00a831007a806b2ba01194f7d3d831a89c7e
nyltf <156117583+nyltf@users.noreply.github.com>
2024-01-11 10:27:38 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,8 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-import os
+import dash
 
-def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Hello, " + name + "!\n"
-    return Response(message)
+app = dash.Dash(__name__)
+
+app.layout = dash.html.Div('Hello world!')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+    app.run(debug=False)
