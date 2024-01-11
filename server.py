1fb1345dcfeb0c157d013f7e8fbdcb31ede9f86a
nyltf <156117583+nyltf@users.noreply.github.com>
2024-01-11 11:09:38 -0500

    Update server.py

--- server.py
+++ server.py
@@ -9,5 +9,5 @@ app.layout = dash.html.Div('Hello world!')
 if __name__ == '__main__':
 #    app.run(debug=False)
     port = int(os.environ.get("PORT"))
-    server = make_server('0.0.0.0', port, app)
+    server = make_server('0.0.0.0', port, app.server)
     server.serve_forever()
