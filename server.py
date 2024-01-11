060b6a9fe443339d0ac6e0c2d16a9f4e5813a0d2
nyltf <156117583+nyltf@users.noreply.github.com>
2024-01-11 13:52:46 -0500

    Update server.py

--- server.py
+++ server.py
@@ -10,5 +10,5 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
-    server = make_server('0.0.0.0', port, app.server)
+    server = make_server('0.0.0.0', port, app)
     server.serve_forever()
